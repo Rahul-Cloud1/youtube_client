0fc42fd20e6737ad13280dcdc72a365635fa9bd8
Your Name <your.email@example.com>
2026-04-07 17:30:55 +0530

    fix: connect login and register routes

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,11 @@ import VideoPlayer from "./pages/VideoPlayer";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 
-
 function App() {
   return (
     <Layout>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<h1>Login Page</h1>} />
         <Route path="/video/:id" element={<VideoPlayer />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
